28d0864ea0c65284dc1ad306c1f978a17cbe2afe
prasanna89654 <poudellprasanna789@gmail.com>
2025-01-24 09:06:38 +0545

    remove one client data

--- users/document-authorization/first.mjs
+++ users/document-authorization/first.mjs
@@ -6,13 +6,6 @@ const users = [
     number: "9812349020",
     passportNo: "PA1190620",
   },
-  {
-    name: "Nishan Pandey",
-    email: "pandeynishan26@gmail.com",
-    password: "Email@590jrt",
-    number: "9812349020",
-    passportNo: "PA1190620",
-  },
 ];
 
 export default users;
