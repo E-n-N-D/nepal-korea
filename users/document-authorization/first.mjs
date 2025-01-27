de9e061cbc7756226185946d5c148b512762b437
prasanna89654 <poudellprasanna789@gmail.com>
2025-01-27 19:26:52 +0545

    latest changes

--- users/document-authorization/first.mjs
+++ users/document-authorization/first.mjs
@@ -1,11 +1,10 @@
 const users = [
   {
-    name: "Rita Nepali",
-    email: "ritanepali1220@gmail.com",
-    password: "wctm faxl mfjm txhe",
+    name: "Rohan Rimal",
+    email: "rohanrimal476@gmail.com",
+    password: "Email@590jrt",
     number: "9812349020",
     passportNo: "PA1190620",
   },
 ];
-
 export default users;
