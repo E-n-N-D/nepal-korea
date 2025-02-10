59ac765a0c1271956a950fed30eb44cf2809bd34
prasanna89654 <poudellprasanna789@gmail.com>
2025-02-10 09:18:16 +0545

    changes

--- users/document-authorization/first.mjs
+++ users/document-authorization/first.mjs
@@ -1,7 +1,7 @@
 const users = [
   {
-    name: "Nishan Pandey",
-    email: "pandeynishan26@gmail.com",
+    name: "Rohan Rimal",
+    email: "rohanrimal476@gmail.com",
     password: "Email@590jrt",
     number: "9812349020",
     passportNo: "PA1190620",
