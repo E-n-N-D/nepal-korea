34fc4da303aa6c720aef5f9349d3ae3678e904fa
prasanna89654 <poudellprasanna789@gmail.com>
2025-02-12 10:00:15 +0545

    changes

--- country.mjs
+++ country.mjs
@@ -53,8 +53,8 @@ const nepalVisa = {
 
 const nepalDoc = {
   emblCd: "NP",
-  businessNm: `{"mainKindNm":["공증· 영사확인(Documents Authentication)"],"cffdnNm":["공증· 영사확인(Documents Authentication)"]}`,
-  grpNmListDB: "Documents Authentication",
+  businessNm: `공증· 영사확인(Documents Authentication)`,
+  grpNmListDB: "공증· 영사확인(Documents Authentication)",
   cffdnNmDB: "Documents Authentication",
   mainKind: "NP0024",
   subKind: "NP0024",
